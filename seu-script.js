2e29683701e5214e0f937625c103da0fde54f3b8
Victortorreto <145171733+Victortorreto@users.noreply.github.com>
2023-12-13 00:43:52 -0300

    Add files via upload

---
+++ seu-script.js
@@ -0,0 +1,11 @@
+
+function enter() {
+    document.getElementById("entrar").style.display = "block"; // Exibindo a outra página
+    alert("Seja Bem-Vindo ao Pizzaria seu Pizza.");
+    window.location.href = "index.html";
+  };
+
+function redirecionar() {
+            alert("Cadastro Bem-Sucedido.");
+        document.getElementById("cadastro").style.display = "block"; // Exibindo a outra página
+    }
